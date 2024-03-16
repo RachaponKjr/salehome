6c6670313db8fbb8d5c31d83c4a5dc1d3dd6c87b
RachaponKJR <rachapon.navy@gmail.com>
2024-03-16 20:08:38 +0700

    update 3/16/2024

--- src/pages/homePage/components/Section1.jsx
+++ src/pages/homePage/components/Section1.jsx
@@ -21,10 +21,13 @@ const Section1 = () => {
       {/* ส่วนหัว มี Input กับ Dropdown */}
       <Flex flexDirection={{base:"column-reverse",md:"row"}} gap={"8px"} justifyContent={"space-between"}>
         {/* Tabs */}
-        <Flex h={'2rem'} gap={'4px'} overflowX={{base:'scroll',md:'auto'}}>
+        <Flex h={'2rem'} overflowX={{base:'scroll',md:'auto'}} borderBottom={"3px solid #EAEAEA"} >
           <Box h={'100%'} width={"max-content"} whiteSpace={"nowrap"} px={4} py={1} fontSize={{ base: "12px", md: "16px" }} cursor={"pointer"} position={"relative"} {...tabs === 0 && { _before: { content: "''", position: "absolute", width: "100%", height: "2px", bottom: "0", left: "0", bg: "blue.500" } }} onClick={() => setTabs(0)}>Lorem, ipsum.</Box>
+          <Divider orientation='vertical' borderLeftWidth={"1px"} />
           <Box h={'100%'} width={"max-content"} whiteSpace={"nowrap"} px={4} py={1} fontSize={{ base: "12px", md: "16px" }} cursor={"pointer"} position={"relative"} {...tabs === 1 && { _before: { content: "''", position: "absolute", width: "100%", height: "2px", bottom: "0", left: "0", bg: "blue.500" } }} onClick={() => setTabs(1)}>Lorem, ipsum.</Box>
+          <Divider orientation='vertical' borderLeftWidth={"1px"}/>
           <Box h={'100%'} width={"max-content"} whiteSpace={"nowrap"} px={4} py={1} fontSize={{ base: "12px", md: "16px" }} cursor={"pointer"} position={"relative"} {...tabs === 2 && { _before: { content: "''", position: "absolute", width: "100%", height: "2px", bottom: "0", left: "0", bg: "blue.500" } }} onClick={() => setTabs(2)}>Lorem, ipsum.</Box>
+          <Divider orientation='vertical' borderLeftWidth={"1px"}/>
           <Box h={'100%'} width={"max-content"} whiteSpace={"nowrap"} px={4} py={1} fontSize={{ base: "12px", md: "16px" }} cursor={"pointer"} position={"relative"} {...tabs === 3 && { _before: { content: "''", position: "absolute", width: "100%", height: "2px", bottom: "0", left: "0", bg: "blue.500" } }} onClick={() => setTabs(3)}>Lorem, ipsum.</Box>
         </Flex>
         <InputGroup w={{base:"100%",md:"15rem"}} size={"sm"} m={{base:'auto',md:'0'}} mb={{base:"8px",md:"0px"}}>
