3f877b9d494e82a5cb96b7d522dbe897cc2c4ccc
RachaponKJR <rachapon.navy@gmail.com>
2024-03-19 23:33:43 +0700

    updatae 3/19/2024

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -6,7 +6,7 @@ import Image from 'next/image'
 
 function Loading() {
   return (
-    <Flex justify={'center'} align={'center'} h={'100vh'} w={'100vw'} position={'fixed'} top={0} left={0} zIndex={1000} bg={'white'}>
+    <Flex justify={'center'} align={'center'} h={'100vh'} w={'100vw'} position={'fixed'} top={0} left={0} zIndex={100000} bg={'white'}>
       <Image src={loading} alt={loading} width={200} height={200} />
     </Flex>
   )
