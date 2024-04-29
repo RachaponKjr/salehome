984d6d826193ea9ba9f56cb809b1880846eafac4
RachaponKJR <rachapon.navy@gmail.com>
2024-04-29 17:13:59 +0700

    tttt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   siteUrl: "https://www.cfasia.co.th",
   generateRobotsTxt: true, // (optional)
-
-}
-;
+  robotsTxtOptions: {
+    additionalSitemaps: ["https://www.cfasia.co.th/sitemap-1.xml"],
+  },
+};
